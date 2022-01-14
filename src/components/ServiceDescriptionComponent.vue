726303c90966d99fbf5ca3496c7d5f579bcab3d3
Francisco Félez Esteban <ffelez@addocean.com>
2022-01-14 19:39:09 +0100

    primeros servicios funcionando

---
+++ src/components/ServiceDescriptionComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="serviceDescriptionComponent">
+    <template>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="text-overline mb-4">{{ services.company }}</div>
+            <v-list-item-title class="text-h5 mb-1">
+              {{ services.type }}
+            </v-list-item-title>
+            <v-list-item-subtitle
+              >Start Time: {{ getValue(services.start_time) }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle
+              >End Time: {{ getValue(services.end_time) }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-avatar class="ma-3" size="125" tile>
+            <v-img
+              :src="require('@/assets/' + getAvatar(services.company) + '')"
+            ></v-img>
+          </v-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn outlined rounded text> Empezar </v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.serviceDescriptionComponent {
+  padding: 20px;
+}
+</style>
+
+<script>
+export default {
+  name: "ServiceDescriptionComponent",
+
+  props: ["services"],
+
+  computed: {
+    message() {
+      return "El identificador del servicio es " + this.services.id;
+    },
+  },
+
+  methods: {
+    getValue(datetime) {
+      if (datetime === null) {
+        return "--";
+      }
+
+      return datetime.toDate().toLocaleString();
+    },
+    getAvatar(company) {
+      if (company == "CEPSA") {
+        return "cepsa.png";
+      }
+      return "vopak.jpeg";
+    },
+  },
+};
+</script>
--- src/components/VisitsListComponent.vue
+++ src/components/VisitsListComponent.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="visitsListComponent">
     <h2>{{ title }}</h2>
-    <v-data-table hide-default-footer :headers="headers" :items="visits">
+    <v-data-table
+      hide-default-footer
+      :headers="headers"
+      :items="visits"
+      show-expand
+    >
       <template v-slot:[`item.eta`]="{ item }">
         <v-chip>
           {{ getValue(item.eta) }}
@@ -20,20 +25,40 @@
         <v-chip>
           {{ getValue(item.atd) }}
         </v-chip>
+      </template>
+      <template v-slot:[`item.services`]="{ item }">
+        <v-chip>
+          {{ item.services }}
+        </v-chip>
+      </template>
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          <div v-if="item.services">
+            <ServiceDescriptionComponent :services="getServices(item)" />
+          </div>
+          <div v-else>No existen servicios asociados a la escala</div>
+        </td>
       </template></v-data-table
     >
   </div>
 </template>
 
 <script>
+import ServiceDescriptionComponent from "./ServiceDescriptionComponent";
 import { db } from "@/fb.js";
 import { collection, onSnapshot } from "firebase/firestore";
 
 export default {
   name: "VisitsListComponent",
   props: ["title"],
+
+  components: {
+    ServiceDescriptionComponent,
+  },
+
   data: () => ({
     visits: [],
+    services: [],
     headers: [
       {
         text: "Escala",
@@ -48,6 +73,7 @@ export default {
       { text: "ATA", value: "ata", sortable: true },
       { text: "ETD", value: "etd", sortable: true },
       { text: "ATD", value: "atd", sortable: true },
+      { text: "Servicios", value: "services", sortable: false },
     ],
   }),
   created() {
@@ -67,6 +93,7 @@ export default {
           this.visits.push({
             ...change.doc.data(),
             id: change.doc.id,
+            services: 0,
           });
           this.visits = this.visits.sort((a, b) => (b.eta < a.eta ? -1 : 1));
         } else if (change.type == "modified") {
@@ -74,6 +101,7 @@ export default {
           this.visits[pos] = {
             ...change.doc.data(),
             id: change.doc.id,
+            services: 0,
           };
 
           this.visits = this.visits.sort((a, b) => (b.eta < a.eta ? -1 : 1));
@@ -83,6 +111,46 @@ export default {
             this.visits = this.visits.splice(this.visits, pos);
             this.visits = this.visits.sort((a, b) => b.estado - a.estado);
           }
+        }
+      });
+    });
+
+    onSnapshot(collection(db, "services"), (querySnapshot) => {
+      const changes = querySnapshot.docChanges();
+
+      changes.forEach((change) => {
+        console.log("service changed", change.doc.data());
+        console.log(change.type);
+        if (change.type == "added") {
+          const visit_id = change.doc.data().visit;
+          console.log(
+            "Nuevo servicio",
+            change.doc.id,
+            "para la escala",
+            visit_id
+          );
+
+          const pos = this.visits.map((val) => val.id).indexOf(visit_id);
+          if (pos >= 0) {
+            console.log("Encontrada escala en posicion", pos);
+            this.visits[pos].services += 1;
+          } else {
+            console.log("No encontrada escala");
+          }
+
+          this.services.push({
+            ...change.doc.data(),
+            id: change.doc.id,
+          });
+        } else if (change.type == "modified") {
+          console.log(
+            "Modificado servicio",
+            change.doc.id,
+            "para la escala",
+            change.doc.data().visit
+          );
+        } else if (change.type == "removed") {
+          console.log("Eliminado servicio", change.doc.id);
         }
       });
     });
@@ -100,6 +168,21 @@ export default {
       }
 
       return datetime.toDate().toLocaleString();
+    },
+    getServices(element) {
+      var service = null;
+      const pos = this.services.map((val) => val.visit).indexOf(element.id);
+      if (pos >= 0) {
+        console.log(
+          "getServicesDescription - Encontrado servicio en posicion",
+          pos
+        );
+        service = this.services[pos];
+      } else {
+        console.log("getServicesDescription - No encontrado servicio");
+      }
+
+      return service;
     },
   },
 };
